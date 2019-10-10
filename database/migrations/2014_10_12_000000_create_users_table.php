7953f6ecbeb9a6056cd0b94b05a80e8ca89d3102
Nuria P <guaupelu@hotmail.com>
2019-10-10 16:32:39 +0200

    updating users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
@@ -53,7 +54,7 @@ class CreateUsersTable extends Migration
         */
 
 
-    
+
     }
 
     /**
